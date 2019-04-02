e81cc45d643734ada631dc78b7c413e42a27acc4
azharmunir43 <azharmunir123@gmail.com>
2019-04-02 10:04:52 +0500

    Added problem description and assumptions.

--- birthday_problem.py
+++ birthday_problem.py
@@ -1,5 +1,13 @@
 # Birthday Problem
 
+# How many people musy be in the room such that the probability of having a pair with same birthday exceeds 50pc?
+
+# Assumptions
+#   1. 365 days in year, no leap year case.
+#   2. Each person's birth is independent, no twins etc.
+#   3. Each day is equally likely.
+
+
 number_of_days_in_year = 365
 
 probability_of_not_having_any_pair = 1
