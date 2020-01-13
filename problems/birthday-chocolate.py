27a24cff17804ca4f8e1351139d9ec0a534920ed
Muhammad Azhar Munir <azharmunir123@gmail.com>
2020-01-13 08:35:49 +0500

    Added birthday chocolate problem

---
+++ problems/birthday-chocolate.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the birthday function below.
+def birthday(s, d, m):
+    # for
+    pass
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    #
+    # n = int(input().strip())
+    #
+    # s = list(map(int, input().rstrip().split()))
+    #
+    # dm = input().rstrip().split()
+    #
+    # d = int(dm[0])
+    #
+    # m = int(dm[1])
+    s = [1, 2, 1, 3, 2]
+    d, m = 3, 2
+    result = birthday(s, d, m)
+
+    # fptr.write(str(result) + '\n')
+    #
+    # fptr.close()
